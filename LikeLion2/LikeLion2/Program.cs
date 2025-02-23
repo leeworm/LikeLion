e4048643a37d64718f41a04733f63443e705bdc0
leeworm <kalrocse@gmail.com>
2025-02-24 00:00:12 +0900

    퀴즈1 까지 해보기

---
+++ LikeLion2/LikeLion2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수 선언
+            int age;                // 정수형 변수 age 선언
+            age = 10;               // 변수에 값을 저장함
+
+            Console.WriteLine(age); // 변수에 저장된 값을 출력
+            /*
+             * WriteLine를 사용하면 해당 값 출력후 Enter(줄 바뀜)
+             * Write를 사용하면 해당 값 출력 후 줄이 바뀌지 않음 !!!!
+             */
+
+            // 3. 문자열
+            Console.WriteLine("3.문자열");
+            string greeting; // 문자열 변수 선언
+            greeting = "Hello, World!"; // 변수에 값을 저장
+            //출력부
+            Console.WriteLine(greeting);    // 출력:Hello, World!
+
+            Console.WriteLine("4.변수 선언과 초기화를 한번에 수행");
+            int score = 100; // 정수형 변수 선언과 동시에 100으로 초기화
+            double temperature = 36.5; // 실수형 변수 선언과 초기화
+            string city = "Seoul"; // 문자열 변수 선언과 초기화
+
+            // 변수 출력
+            Console.WriteLine(score); // 출력: 100
+            Console.WriteLine(temperature); // 출력: 36.5
+            Console.WriteLine(city); // 출력: Seoul
+
+            // 같은 데이터 타입의 변수를 쉼표로 구분해 한 번에 선언
+            int x = 10, y = 20, z = 30; // 정수형 변수 x, y, z를 선언하고 각각 초기화
+            // 출력
+            Console.WriteLine(x); // 출력: 10
+            Console.WriteLine(y); // 출력: 20
+            Console.WriteLine(z); // 출력: 30
+
+            // 6.상수 사용하기
+            // 상수: 값을 변경할 수 없는 변수
+            const double Pi = 3.14159; // 상수 Pi 선언 및 초기화
+            const int MaxScore = 100; // 정수형 상수 선언
+
+            // 출력
+            Console.WriteLine("Pi:" + Pi); // 출력:  Pi = 3.14159
+            Console.WriteLine("Max Score:" + MaxScore); // 출력 MaxScore = 100
+
+            // Pi = 3.14; // 오류 발생! 상수는 값을 변경할 수 없습니다.
+
+            // 퀴즈!
+
+            int att = 16755, hp = 78103, stat1 = 36, stat2 = 1017, stat3 = 41, stat4 = 611, stat5 = 22, stat6 = 39;
+
+           Console.WriteLine("기본 특성");
+           Console.WriteLine("---------------");
+           Console.WriteLine("공격력:" + att);
+           Console.WriteLine("최대 생명력:" + hp);
+
+           Console.WriteLine("전투 특성");
+           Console.WriteLine("---------------");
+           Console.WriteLine("치명:" + stat1);
+           Console.WriteLine("특화:" + stat2);
+           Console.WriteLine("제압:" + stat3);
+           Console.WriteLine("신속:" + stat4);
+           Console.WriteLine("인내:" + stat5);
+           Console.WriteLine("숙련:" + stat6);
+
+        }
+    }
+}
---
+++ Study3/Study3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 리터럴: 코드에서 고정된 값을 의미합니다.
+
+            //int number = 10; // 정수형 리터럴
+            //double pi = 3.14; // 실수형 리터럴
+            //char letter = 'A'; // 문자 리터럴
+            //string name = "Alice"; // 문자열 리터럴
+
+            //Console.WriteLine(number); // 출력: 10
+            //Console.WriteLine(pi); // 출력: 3.14
+            //Console.WriteLine(letter); // 출력: A
+            //Console.WriteLine(name); // 출력: Alice
+
+            /*
+             *
+             * 강사님 말씀 : double 대신 Float 자주 사용함
+             * - 문자 하나 넣을 때, [ ex) A ] ' '를 사용하고,
+             * 단어 하나 넣을 때, [ ex) Apple ] " "를 사용합니다.
+             * 
+             * 
+             */
+
+            /*깜짝 과제
+             * 캐릭터
+             * hp : 100
+             * att : 56.7
+             * 캐릭터이름 : ???
+             * 등급 : S
+             */
+
+            int hp = 100;
+            double att = 56.7;
+            String ChName = "김빵빵";
+            char grade = 'S';
+
+            Console.WriteLine("캐릭터");
+            Console.WriteLine("HP:" + hp);
+            Console.WriteLine("공격력:" + att);
+            Console.WriteLine("이름:" + ChName);
+            Console.WriteLine("등급:" + grade);
+
+
+        }
+    }
+}
